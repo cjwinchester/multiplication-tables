af58ee078493e620534e395c315c83d5894c8d25
Cody Winchester <cjwinchester@gmail.com>
2020-04-02 13:54:06 -0600

    add clear button

--- scripts.js
+++ scripts.js
@@ -1,21 +1,32 @@
 const cells = document.querySelectorAll('tr:not(:first-of-type) td:not(:first-of-type)');
 
+const button = document.getElementById('clear');
+
 const score = document.getElementById('score');
 const total_pct = document.getElementById('total_pct');
 
 let count_correct = 0;
 const total = 144;
 
-for (let i=0; i<cells.length; i++) {  
+button.addEventListener('click', function(e) {
+  count_correct = 0;
+  score.innerHTML = 0;
+  total_pct.innerHTML = 0;
+  for (let i=0; i<cells.length; i++) {
+    cells[i].style.backgroundColor = '#fff';
+  }
+});
+
+for (let i=0; i<cells.length; i++) {
  
   cells[i].addEventListener('click', function(e) {
     if (this.dataset.checked === 'x') {
       this.dataset.checked = '';
-      this.style.background = '#fff';
+      this.style.backgroundColor = '#fff';
       count_correct -=1;
     } else {
       this.dataset.checked = 'x';
-      this.style.background = '#9AFF95';
+      this.style.backgroundColor = '#9AFF95';
       count_correct +=1;
     }
     score.innerHTML = count_correct;
